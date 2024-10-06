86bf7062731a46ae6308831203c14aac8e637ea5
alabo-excel <iamalaboexcel@gmail.com>
2024-10-06 14:47:25 +0100

    claim NFT warning

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,14 @@
 "use client";
 
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { useAccount, } from 'wagmi'
+import Modal from './Modal';
 
 const Card = ({ claimNft }) => {
+  const account = useAccount()
+  const [show, setShow] = useState(false)
+
   return (
     <div className='card lg:w-[24%]'>
       <img className='w-full' src="/images/academy.png" alt="NFT" />
@@ -15,7 +20,7 @@ const Card = ({ claimNft }) => {
           <p className='text-lg font-bold'>3 ETH</p> */}
         </div>
         {claimNft ? (
-          <button onClick={() => claimNft()} className='w-full btn p-4 rounded-full'>
+          <button onClick={() => { account.isConnected ? claimNft() : setShow(true) }} className='w-full btn p-4 rounded-full'>
             Claim NFT
           </button>
         ) : (
@@ -26,6 +31,10 @@ const Card = ({ claimNft }) => {
           </Link>
         )}
       </div>
+      <Modal isOpen={show} onClose={() => setShow(false)}>
+        <h2 className="text-2xl text-[#0D0516] text-center font-semibold mb-4">Hello there!</h2>
+        <p className="mb-4 text-[#0D0516] text-xl text-center">It looks like you tried to claim the NFT without connecting your wallet. Please click "Connect Wallet" in the header to continue and complete your claim.</p>
+      </Modal>
     </div>
   );
 };
