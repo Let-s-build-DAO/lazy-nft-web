8feb6a4ff12a45561b8b2bc383e0b3c1314b861a
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-24 15:05:18 +0100

    local build working

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -3,18 +3,18 @@
 import React, { useState, useEffect } from 'react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
-// import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useWeb3Modal } from '@web3modal/wagmi/react';
 import { useDisconnect, useAccount } from 'wagmi';
 import { readContract } from '@wagmi/core';
 import MarkeplaceAbi from '../utils/marketPlaceAbi';
 import { MARKETPLACE_CONTRACT } from '@/config/constants';
 import { config } from '@/utils/wagmi';
 
-import dynamic from 'next/dynamic';
-const { useWeb3Modal} = dynamic(
-  () => import("@web3modal/wagmi/react").then((mod) => mod.Web3Modal),
-  { ssr: false }
-);
+// import dynamic from 'next/dynamic';
+// const { useWeb3Modal} = dynamic(
+//   () => import("@web3modal/wagmi/react").then((mod) => mod.useWeb3Modal),
+//   { ssr: false }
+// );
 
 const HeaderNav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
