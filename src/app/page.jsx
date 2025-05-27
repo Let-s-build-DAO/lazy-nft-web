28f099de8f3f9160a5d430b65611f367007ffc48
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-27 14:43:09 +0100

    refactor: remove Providers component and integrate its functionality directly in Home for improved structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,38 @@
-import dynamic from 'next/dynamic';
+'use client'
 
+
+import dynamic from 'next/dynamic';
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { config } from "@/utils/wagmi";
+import { projectId } from "@/config";
+import { cookieToInitialState } from 'wagmi'
 const HeaderNav = dynamic(() => import('@/components/HeaderNav'), { ssr: false });
 const Hero = dynamic(() => import('@/components/Hero'), {
   ssr: false, // Disable SSR for this component
 });
-const Providers = dynamic(() => import('./provider'), { ssr: false })
+
+const queryClient = new QueryClient();
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
 
 export default function Home() {
+  const initialState = cookieToInitialState(config)
+
   return (
     <main className="">
-      <Providers>
-        <HeaderNav />
-        <Hero />
-      </Providers>
+      <WagmiProvider config={config} initialState={initialState}>
+        <QueryClientProvider client={queryClient}>
+          <HeaderNav />
+          <Hero />
+        </QueryClientProvider>
+      </WagmiProvider>
     </main>
   );
 }
