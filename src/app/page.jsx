4b9187c15dc30886d16a58e1f3d346be542d7f70
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-27 15:49:19 +0100

    refactor: comment out unused HomeComp import and streamline createWeb3Modal initialization

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
-import HomeComp from '@/components/HomeComp';
+// import HomeComp from '@/components/HomeComp';
 import React from 'react';
+import dynamic from 'next/dynamic';
+const HomeComp = dynamic(() => import('@/components/HomeComp'), { ssr: false });
 
 const Home = () => {
   return (
