132205d41593ce8b307dd77f2039ba8393a683ca
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-27 14:30:49 +0100

    refactor: update layout to use dynamic imports for HeaderNav and Providers, and adjust FooterNav copyright year

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,9 @@
 
 import "./globals.css";
 import dynamic from 'next/dynamic';
-const HeaderNav = dynamic(() => import('@/components/HeaderNav'), { ssr: false });
-
 import FooterNav from "@/components/FooterNav"
 // import HeaderNav from "@/components/HeaderNav"
 // import Providers from "./provider";
-const Providers = dynamic(() => import('./provider'), { ssr: false })
 
 
 export const metadata = {
@@ -18,16 +15,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Providers>
+        {/* <Providers> */}
           <main>
-
-            <HeaderNav />
             <div className="">
               {children}
             </div>
             <FooterNav />
           </main>
-        </Providers>
+        {/* </Providers> */}
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,18 @@
 import dynamic from 'next/dynamic';
 
-const Hero = dynamic(() => import('@/components/Hero'), { 
+const HeaderNav = dynamic(() => import('@/components/HeaderNav'), { ssr: false });
+const Hero = dynamic(() => import('@/components/Hero'), {
   ssr: false, // Disable SSR for this component
 });
+const Providers = dynamic(() => import('./provider'), { ssr: false })
 
 export default function Home() {
   return (
-    <main className="lg:px-10 px-4">
-      <Hero />
+    <main className="">
+      <Providers>
+        <HeaderNav />
+        <Hero />
+      </Providers>
     </main>
   );
 }
