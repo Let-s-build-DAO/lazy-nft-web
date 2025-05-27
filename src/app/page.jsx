09e4e6e3f1851857a6adb838ad1b6c0949969465
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-27 19:33:10 +0100

    redeploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,26 @@
-// import HomeComp from '@/components/HomeComp';
-import React from 'react';
-import dynamic from 'next/dynamic';
-import Loader from '@/components/Loader';
+'use client';
 
+import React, { useEffect, useState } from 'react';
+import Loader from '@/components/Loader';
 import HomeComp from '@/components/HomeComp';
-// const HomeComp = dynamic(() => import('@/components/HomeComp'), {
-//   ssr: false,
-//   loading: () => <Loader />,
-// });
 
 const Home = () => {
-  return (
-    <HomeComp />
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay for effect (optional)
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 500); // You can adjust or remove delay
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return <HomeComp />;
 };
 
 export default Home;
