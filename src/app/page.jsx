1ff94c4dd3b69b71f58f6ad3cb8d667e2bd5d505
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-27 14:49:09 +0100

    refactor: consolidate Home component structure and remove unused Card and Claim components for improved clarity

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,38 +1,10 @@
-'use client'
+import HomeComp from '@/components/HomeComp';
+import React from 'react';
 
-
-import dynamic from 'next/dynamic';
-import { WagmiProvider } from "wagmi";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createWeb3Modal } from '@web3modal/wagmi/react'
-import { config } from "@/utils/wagmi";
-import { projectId } from "@/config";
-import { cookieToInitialState } from 'wagmi'
-const HeaderNav = dynamic(() => import('@/components/HeaderNav'), { ssr: false });
-const Hero = dynamic(() => import('@/components/Hero'), {
-  ssr: false, // Disable SSR for this component
-});
-
-const queryClient = new QueryClient();
-
-createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
-})
-
-export default function Home() {
-  const initialState = cookieToInitialState(config)
-
+const Home = () => {
   return (
-    <main className="">
-      <WagmiProvider config={config} initialState={initialState}>
-        <QueryClientProvider client={queryClient}>
-          <HeaderNav />
-          <Hero />
-        </QueryClientProvider>
-      </WagmiProvider>
-    </main>
+    <HomeComp />
   );
-}
+};
+
+export default Home;
---
+++ src/components/HomeComp.jsx
@@ -0,0 +1,38 @@
+'use client'
+
+
+import dynamic from 'next/dynamic';
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { config } from "@/utils/wagmi";
+import { projectId } from "@/config";
+import { cookieToInitialState } from 'wagmi'
+const HeaderNav = dynamic(() => import('./HeaderNav'), { ssr: false });
+const Hero = dynamic(() => import('./Hero'), {
+  ssr: false, // Disable SSR for this component
+});
+
+const queryClient = new QueryClient();
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+export default function HomeComp() {
+  const initialState = cookieToInitialState(config)
+
+  return (
+    <main className="">
+      <WagmiProvider config={config} initialState={initialState}>
+        <QueryClientProvider client={queryClient}>
+          <HeaderNav />
+          <Hero />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </main>
+  );
+}
