8c5a3c510abb706855fdfd391e29af0ab3d9a90e
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-28 10:00:26 +0100

    redeploy

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 import Loader from '@/components/Loader';
 // import HomeComp from '@/components/HomeComp';
 const HomeComp = dynamic(() => import('@/components/HomeComp'), {
-  ssr: false,
+  ssr: true,
   loading: () => <Loader />,
 });
 
