a890fcde5471b01f39886bdadabbb977b3cfb489
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-28 09:16:16 +0100

    deploy

--- src/components/HomeComp.jsx
+++ src/components/HomeComp.jsx
@@ -1,5 +1,7 @@
 'use client'
 
+import React, { useEffect, useState } from 'react';
+import Loader from './Loader';
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createWeb3Modal } from '@web3modal/wagmi/react'
@@ -12,15 +14,32 @@ import Hero from './Hero'
 
 const queryClient = new QueryClient();
 
-createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
-})
+// createWeb3Modal({
+//   wagmiConfig: config,
+//   projectId,
+//   enableAnalytics: true, // Optional - defaults to your Cloud configuration
+//   enableOnramp: true // Optional - false as default
+// })
 
 export default function HomeComp() {
   const initialState = cookieToInitialState(config)
+
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      createWeb3Modal({
+        wagmiConfig: config,
+        projectId,
+        enableAnalytics: true,
+        enableOnramp: true,
+      })
+      setMounted(true)
+    }
+  }, [])
+
+  if (!mounted) return <Loader /> // or show a loader here
+
 
   return (
     <main className="">
