7920f4872af8e10dbf70f526a4bacee0b2560164
iamalaboexcel <iamalaboexcel@gmail.com>
2025-05-28 09:52:23 +0100

    refactor: clean up imports and enhance Web3Modal configuration

--- src/components/HomeComp.jsx
+++ src/components/HomeComp.jsx
@@ -1,12 +1,8 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
-import Loader from './Loader';
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createWeb3Modal } from '@web3modal/wagmi/react'
 import { config } from "@/utils/wagmi";
-import { projectId } from "@/config";
 import { cookieToInitialState } from 'wagmi'
 import FooterNav from './FooterNav';
 import HeaderNav from './HeaderNav'
@@ -14,23 +10,9 @@ import Hero from './Hero'
 
 const queryClient = new QueryClient();
 
+
 export default function HomeComp() {
   const initialState = cookieToInitialState(config)
-
-  const [mounted, setMounted] = useState(false)
-
-  createWeb3Modal({
-    wagmiConfig: config,
-    projectId,
-    enableAnalytics: true, // Optional - defaults to your Cloud configuration
-    enableOnramp: true // Optional - false as default
-  })
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) return <Loader /> // or show a loader here
 
 
   return (
--- src/utils/wagmi.js
+++ src/utils/wagmi.js
@@ -1,10 +1,18 @@
 import { http, createConfig } from '@wagmi/core'
 import { liskSepolia, lisk, rootstockTestnet, rootstock } from '@wagmi/core/chains'
-import { injected  } from '@wagmi/connectors'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
 
+export const projectId = process.env.NEXT_PUBLIC_CONNECT_WALLET_ID
 
 export const config = createConfig({
   chains: [lisk, liskSepolia, rootstock, rootstockTestnet],
+  connectors: [
+    injected(),
+    walletConnect({ projectId }),
+    metaMask(),
+    safe(),
+  ],
   transports: {
     [lisk.id]: http(),
     [liskSepolia.id]: http(),
@@ -12,4 +20,11 @@ export const config = createConfig({
     [rootstockTestnet.id]: http(),
   },
   ssr: true
+})
+
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
 })
