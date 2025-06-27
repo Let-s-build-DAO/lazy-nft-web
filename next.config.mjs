05c228f705a0268ffd5311b2b18acb26ac958e4c
iamalaboexcel <iamalaboexcel@gmail.com>
2025-06-27 19:00:06 +0100

    deployment fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 // next.config.mjs
 
-import webpack from 'webpack';
 
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-    config.plugins.push(new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
-    }));
-    return config;
-  }
 }
 
 export default nextConfig;
